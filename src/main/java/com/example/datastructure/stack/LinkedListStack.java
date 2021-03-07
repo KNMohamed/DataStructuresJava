ba18bc9b7e2388467857f34dd3e8c207ad4d03b4
Khalid Mohamed <khalid.tcs.mohamed@sunlife.com>
2021-03-07 14:32:47 -0500

    Documentation for LinkedList stack implementation

--- src/main/java/com/example/datastructure/stack/LinkedListStack.java
+++ src/main/java/com/example/datastructure/stack/LinkedListStack.java
@@ -2,13 +2,22 @@ package com.example.datastructure.stack;
 
 import com.example.adt.stack.Stack;
 import com.example.datastructure.linkedlist.SinglyLinkedList.SingleLinkedList;
-
 import java.util.EmptyStackException;
 
+
+/**
+ * @author Khalid Mohames
+ * A Generic Stack implementation using a LinkedList
+ */
 public class LinkedListStack<T extends Comparable<T>> implements Stack<T> {
     private SingleLinkedList<T> linkedList = new SingleLinkedList<>();
     int count;
 
+    /**
+     * @return The last item that was added to the Stack (LIFO)
+     * Time Complexity: O(1)
+     * @throws EmptyStackException
+     */
     @Override
     public T pop() throws EmptyStackException {
         if(count == 0) throw new EmptyStackException();
@@ -18,12 +27,22 @@ public class LinkedListStack<T extends Comparable<T>> implements Stack<T> {
         return data;
     }
 
+    /**
+     * Pushes the data to the stack
+     * Time Complexity: O(1)
+     * @param data
+     */
     @Override
     public void push(T data) {
         linkedList.insert(data);
         count = linkedList.size();
     }
 
+    /**
+     * Peeks at the item from the top of the stack without removing it
+     * Time Complexity O(1)
+     * @return
+     */
     @Override
     public T peek() {
         if(count == 0) throw new EmptyStackException();
@@ -31,11 +50,21 @@ public class LinkedListStack<T extends Comparable<T>> implements Stack<T> {
         return data;
     }
 
+    /**
+     * Checks if the stack is empty
+     * Time Complexity O(1)
+     * @return
+     */
     @Override
     public boolean isEmpty() {
         return count==0 ? true : false;
     }
 
+    /**
+     * Returns the size of the stack
+     * Time Complexity O(1)
+     * @return
+     */
     @Override
     public int size() {
         return count;
