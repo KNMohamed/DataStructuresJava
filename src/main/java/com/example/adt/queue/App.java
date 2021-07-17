30452896be42d08fb87dbc6f208372f6e606f081
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2021-07-17 00:38:45 -0400

    Confirm Queue linked list impl works

--- src/main/java/com/example/adt/queue/App.java
+++ src/main/java/com/example/adt/queue/App.java
@@ -1,17 +1,17 @@
 package com.example.adt.queue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class App {
     public static void main(String[] args) {
-        Queue<Integer> queue = new Queue<>();
-        queue.enqueue(5);
-        queue.enqueue(6);
-        queue.enqueue(7);
-        queue.enqueue(8);
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.add(5);
+        q.add(9);
+        q.add(12);
 
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-
+        while(!q.isEmpty()){
+            System.out.println(q.remove());
+        }
     }
 }
